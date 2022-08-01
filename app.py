6c8f4e6e7816b3493578f9e2aceca3c2ff377151
Claudinei Pereira <contato@claudineipereira.com>
2022-08-01 17:37:48 -0300

    Adiciona endpoint receitas (get & post)

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from datetime import datetime
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+MSG_NOT_FOUND = 'Não encontrado.'
+MSG_NO_DATA = 'Dados não informados'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///backend.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
+
+# Modelos de banco de dados
 class Receitas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    descricao = db.Column(db.String(255), nullable=False)
+    descricao = db.Column(db.String(255), unique=True, nullable=False)
     valor = db.Column(db.Float, nullable=False)
     data = db.Column(db.DateTime, server_default=db.func.current_timestamp(), 
                         server_onupdate=db.func.current_timestamp(), nullable=False)
@@ -20,7 +26,7 @@ class Receitas(db.Model):
 
 class Despesas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    descricao = db.Column(db.String(255), nullable=False)
+    descricao = db.Column(db.String(255), unique=True, nullable=False)
     valor = db.Column(db.Float, nullable=False)
     data = db.Column(db.DateTime, server_default=db.func.current_timestamp(), 
                         server_onupdate=db.func.current_timestamp(), nullable=False)
@@ -29,9 +35,52 @@ class Despesas(db.Model):
         return f'<Despesa {id}'
 
 
-@app.route('/')
-def index():
-    return 'Olá!'
+# Esquemas da API
+class ReceitasSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Receitas
+    
+
+class DespesasSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Despesas
+
+
+receita_schema = ReceitasSchema()
+receitas_schema = ReceitasSchema(many=True)
+
+despesa_schema = DespesasSchema()
+despesas_schema = DespesasSchema(many=True)
+
+
+@app.route('/api/receitas', methods=['GET'])
+def get_receitas():
+    todas_receitas = Receitas.query.all()
+    if todas_receitas:
+        return jsonify(receitas_schema.dump(todas_receitas)), 200
+    else:
+        return jsonify({
+            'Mensagem': MSG_NOT_FOUND   
+        }), 404
+
+
+@app.route('/api/receitas', methods=['POST'])
+def post_receitas():
+    descricao = request.json.get('descricao', '')
+    valor = request.json.get('valor', '')
+
+    receita = Receitas(
+        descricao=descricao, 
+        valor=valor
+    )
+
+    try:
+        db.session.add(receita)
+        db.session.commit()
+    except:
+        db.session.rollback()
+
+    return jsonify(receita_schema.dump(receita)), 201
 
 
 if __name__ == '__main__':
